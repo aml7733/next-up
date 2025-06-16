5b1b2a507c77109beb11cd8fd3f10119097d686d
Adam Lucia <lucia.adam@gmail.com>
2025-06-16 01:08:29 -0400

    add configuration files and scripts for ESLint, Prettier, and VSCode tasks

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['expo', '@react-native-community'],
+  rules: {
+    // Customize rules as needed
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-native/no-inline-styles': 'warn',
+  },
+};
